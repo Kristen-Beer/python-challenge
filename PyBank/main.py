5721af1816afd3912b032bf17e46b9b3cbf230e4
Kristen-Beer <Krismason56@gmail.com>
2023-07-18 20:46:23 -0700

    Added output to text file command

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+#Find average change in Profits/losses
 import csv
 
 budgetdata_csv = r"C:\\Users\krism\Documents\School\Module3Challenge\python-challenge\Starter_Code\PyBank\Resources\\budget_data.csv"
@@ -12,7 +13,30 @@ with open(budgetdata_csv) as csv_file:
 
     # Count months
     count = 0
+    #variable to store the sum of the second column
+    total_Profits_losses = 0
+    #Variable to store the average change
+    average_change = 0
+    #variable to store the max profits/losses value
+    greatest_increase = None
+
+    
     for row in csv_reader:
+        #Total months
         count += 1
 
-print("Total Months =", count)
+        #Net total
+        net_total = int(row[1])
+        total_Profits_losses += net_total
+
+        #Average change is the total / number of months
+        average_change = int(row[1])
+        final_average_change = int(total_Profits_losses/count)
+        
+    print("Financial Analysis")
+    print("Total months =", count)
+    print("Total:", total_Profits_losses)
+    print("Average Change:", final_average_change)
+
+    #Specify the file to write to
+    
